9c4f3fcfed0964aeb0238d22c43a561c5f8c3e53
rajdhanani <43428556+dhananiraj@users.noreply.github.com>
2019-07-02 09:04:01 +0530

    Update INS_P2.java

--- INS_P2.java
+++ INS_P2.java
@@ -6,42 +6,55 @@ import java.util.Scanner;
  *
  * @author Raj Dhanani
  */
+class Caesar {
 
-class Caesar{
-    
 //a-97
 //z-122
 //A-65
 //Z-90
-    
-    String plainText="";
-    String cipherText="";
+    String plainText = "";
+    String cipherText = "";
     int key;
-    Caesar(){
-        
+
+    Caesar() {
+
     }
-    Caesar(String plainText,int key){
+
+    Caesar(String plainText, int key) {
         this.plainText = plainText;
         this.key = key;
         encrypt();
     }
-    void encrypt(){
-        for(int i = 0; i < plainText.length() ; i++){
+
+    void encrypt() {
+        for (int i = 0; i < plainText.length(); i++) {
             char p = plainText.charAt(i);
-            if(p >= 'A' && p <= 'Z'){
-                 cipherText += (char)(((p - 64) + key) % 27 + 64);
+            if (p >= 'A' && p <= 'Z') {
+                cipherText += (char) (((p - 64) + key) % 27 + 64);
+            } else if (p >= 'a' && p <= 'z') {
+                cipherText += (char) (((p - 96) + key) % 27 + 96);
+            } else {
+                cipherText += p;
             }
-            else if (p >= 'a' && p <= 'z'){
-                 cipherText += (char)(((p - 96) + key) % 27 + 96);
-            }
-            else{
-                 cipherText += p;
-            } 
         }
     }
-    
-}
 
+    String decrypt(String cipherText, int key) {
+        String pT = "";
+        for (int i = 0; i < cipherText.length(); i++) {
+            char p = cipherText.charAt(i);
+            if (p >= 'A' && p <= 'Z') {
+                pT += (char) (((p - 64) - key) % 27 + 64);
+            } else if (p >= 'a' && p <= 'z') {
+                pT += (char) (((p - 96) - key) % 27 + 96);
+            } else {
+                pT += p;
+            }
+        }
+        return pT;
+    }
+
+}
 
 public class INS_P2 {
 
@@ -52,9 +65,10 @@ public class INS_P2 {
         String input = in.nextLine();
         System.out.println("Enter key:");
         int key = in.nextInt();
-        Caesar c = new Caeser(input,key);
-        System.out.println("Cipher:"+c.cipherText);
+        Caesar c = new Caesar(input, key);
+        System.out.println("Cipher:" + c.cipherText);
+        System.out.println("Decripted:" + c.decrypt(c.cipherText,c.key));
 
     }
-    
+
 }
