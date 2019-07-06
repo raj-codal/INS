d58fa5e25d47e24618d4c5ff2a8988098aa076b4
rajdhanani <43428556+dhananiraj@users.noreply.github.com>
2019-07-06 20:55:01 +0530

    minimized version

--- INS_P2.java
+++ INS_P2.java
@@ -41,35 +41,19 @@ class Caesar {
 
     String decrypt(String cipherText, int key) {
         String pT = "";
-        char e;
+        int c, t;
         for (int i = 0; i < cipherText.length(); i++) {
-            char p = cipherText.charAt(i);
-            if (p >= 'A' && p <= 'Z') {
-                int temp = (int) (((p - 65) - key) % 26 + 65);
-                if(temp < 65){
-                    e = (char) ((int)'Z' - (65 - temp));
-                    System.out.println((int)e);
-                }
-                else{
-                    e = (char)temp;
-                }
-            } else if (p >= 'a' && p <= 'z') {
-                int temp = (char) (((p - 97) - key) % 26 + 97);
-                if(temp < 97){
-                    e = (char) ((int)'z' - (97 - temp));
-                    System.out.println((int)e);
-                }
-                else{
-                    e = (char)temp;
-                }
+            c = cipherText.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                pT += (char) ((((t = (c - 'a' - key) % 26) < 0) ? t + 26 : t) + 'a');
+            } else if (c >= 'A' && c <= 'Z') {
+                pT += (char) ((((t = (c - 'A' - key) % 26) < 0) ? t + 26 : t) + 'A');
             } else {
-                e = p;
+                pT += (char) c;
             }
-            pT += e;
         }
         return pT;
     }
-
 }
 
 public class INS_P2 {
@@ -83,7 +67,7 @@ public class INS_P2 {
         int key = in.nextInt();
         Caesar c = new Caesar(input, key);
         System.out.println("Cipher:" + c.cipherText);
-        System.out.println("Decripted:" + c.decrypt(c.cipherText,c.key));
+        System.out.println("Decripted:" + c.decrypt(c.cipherText, c.key));
 
     }
 
